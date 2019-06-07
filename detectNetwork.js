279e60b397921b6b1316162e06c4101ca296ae4d
Jason Chen <jason.sl.chen@gmail.com>
2019-06-07 09:50:32 -0700

    Update detectNetwork for China UnionPay and Switch

--- detectNetwork.js
+++ detectNetwork.js
@@ -23,13 +23,41 @@ var detectNetwork = function(cardNumber) {
     return 'American Express';
   } else if (length === 16 && (firstTwo === '51' || firstTwo === '52' || firstTwo === '53' || firstTwo === '54' || firstTwo === '55')) {
     return 'MasterCard';
-  } else if ((length === 13 || length === 16 || length === 19) && firstTwo[0] === '4') {
-    return 'Visa';
+  } else if (cardNumber[0] === '4') {
+    if ((cardNumber.slice(1, 4) === '903' || cardNumber.slice(1, 4) === '905' || cardNumber.slice(1, 4) === '911' || cardNumber.slice(1, 4) === '936') && (length === 16 || length === 19 || length --- 18)) {
+      return 'Switch';
+    } else if (length === 13 || length === 16 || length === 19) {
+      return 'Visa';
+    }
+  } else if (cardNumber.slice(0, 6) === '564182' || cardNumber.slice(0, 6) === '633110' || cardNumber.slice(0, 4) === '6333' || cardNumber.slice(0, 4) === '6759') {
+    return 'Switch';
   } else if ((length === 16 || length === 19) && (cardNumber.slice(0, 4) === '6011' || (cardNumber.slice(0, 3) >= '644' && cardNumber.slice(0, 3) <= '649') || firstTwo === '65' )) {
     return 'Discover';
   } else if ((length >= 12 && length <= 19) && (cardNumber.slice(0, 4) === '5018' || cardNumber.slice(0, 4) === '5020' || cardNumber.slice(0, 4) === '5038' || cardNumber.slice(0, 4) === '6304')) {
     return 'Maestro';
+  } else if (ChinaUnionPay(cardNumber.slice(0, 6), length)) {
+    return 'China UnionPay';
   }
 };
 
+
+var ChinaUnionPay = function(card, length) {
+
+  for (let prefix = 624; prefix <= 626; prefix++) {
+    if ((length >= 16 && length <= 19) && prefix.toString() === card.slice(0, 3)) {
+      return true;
+    } 
+  }
+  for (let prefix = 622126; prefix <= 622925; prefix++) {
+    if ((length >= 16 && length <= 19) && prefix.toString() === card) {
+      return true;
+    } 
+  }
+  
+  for (let prefix = 6282; prefix <= 6288; prefix++) {
+    if ((length >= 16 && length <= 19) && prefix.toString() === card.slice(0, 4)) {
+      return true;
+    } 
+  }
+}
 
